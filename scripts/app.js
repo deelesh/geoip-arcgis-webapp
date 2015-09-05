4562f9708a6d01c0a9fcd08a4737f76cd9204e58
Deelesh Mandloi <deelesh@gmail.com>
2015-09-05 12:14:58 -0700

    First working version
    First working version

---
+++ scripts/app.js
@@ -0,0 +1,46 @@
+﻿require([
+	'dojo/request/script',
+	'esri/map',
+	'dojo/domReady!'
+], function (script, Map) {
+
+    //Setup default map options
+    var map;
+    var mapOptions = {
+        center: [0, 0],
+        zoom: 3,
+        basemap: "streets"
+    };
+
+    var mapCenteredAtGeoIP = function (mapDiv) {
+
+        //Setup options to make AJAX requests
+        var url = "";
+        var xhrOptions = {
+            jsonp: "callback",
+            query: {
+                f: "json"
+            }
+        };
+
+        var handleAjaxError = function (error) {
+            console.error("error performing XHR to " + error.response.url);
+            //Initialize the map with default options
+            map = new Map(mapDiv, mapOptions);
+        };
+
+        //Get the Client IP
+        url = "https://www.arcgis.com/sharing/geoip.jsp";
+        script.get(url, xhrOptions).then(function (response) {
+            var clientIP = response.clientip || "www.arcgis.com";
+            //clientIP = "www.arcgis.com";
+            //Perform a GeoIP lookup
+            url = "https://freegeoip.net/json/" + clientIP;
+            script.get(url, xhrOptions).then(function (response) {
+                mapOptions.center = [response.longitude, response.latitude];
+                mapOptions.zoom = mapOptions.zoom * 4;
+                map = new Map(mapDiv, mapOptions);
+            }, handleAjaxError);
+        }, handleAjaxError);
+    } ("mapDiv");
+});
